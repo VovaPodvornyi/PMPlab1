5f837d5be38183340761103bb076ea5da972aed0
VovaPodvornyi <107669288+VovaPodvornyi@users.noreply.github.com>
2024-01-17 10:24:42 +0200

    Add files via upload

---
+++  details.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:podvorniy/person.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key, required this.person});
+
+  final Person person;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Fullname: " + person.fullName),
+        ),
+        body: Center(
+          child: Text ("Position: " + person.position),
+        )
+    );
+  }
+}
